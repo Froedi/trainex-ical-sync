ba777f4a85cb1b7cfb6bd5e9204b2559b6299b21
Froedi <47183654+Froedi@users.noreply.github.com>
2026-02-17 12:30:25 +0100

    Update script.py

--- script.py
+++ script.py
@@ -5,11 +5,9 @@ from playwright.sync_api import sync_playwright
 USERNAME = os.getenv("TRAINEX_USER")
 PASSWORD = os.getenv("TRAINEX_PASS")
 
-# Login-URL und iCal-Seite
 LOGIN_URL = "https://trex.phwt.de/phwt-trainex//login.cfm"
 ICAL_PAGE_URL = "https://trex.phwt.de/phwt-trainex//cfm/einsatzplan/einsatzplan_listenansicht_iCal.cfm?TokCF19=0T1320846724&IDphp17=3P846724&sec18m=7S208467241320846724&1771320846877&utag=17&umonat=2&ujahr=2026&ics=1"
 
-# Speicherort
 OUTPUT_FILE = Path("stundenplan.ics")
 
 def download_ical():
@@ -20,17 +18,19 @@ def download_ical():
 
         # Login
         page.goto(LOGIN_URL)
-        page.fill("input[name='username']", USERNAME)  # ggf. anpassen
-        page.fill("input[name='password']", PASSWORD)  # ggf. anpassen
-        page.click("input[type='submit']")
-        page.wait_for_load_state("networkidle")
+        page.wait_for_selector("#Login", timeout=60000)  # 60 Sekunden warten
+
+        page.fill("#Login", USERNAME)
+        page.fill("#Passwort", PASSWORD)
+        page.click("#btnanm")
+        page.wait_for_load_state("networkidle")  # warten bis die Seite geladen ist
 
         # iCal-Download-Seite
         page.goto(ICAL_PAGE_URL)
 
         # Download der Datei
         with page.expect_download() as download_info:
-            page.click("text=iCal")  # wenn nötig, ggf. anpassen
+            page.click("text=iCal")  # klickt den iCal-Link
 
         download = download_info.value
         download.save_as(OUTPUT_FILE)
